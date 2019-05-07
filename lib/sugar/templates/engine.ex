20efbf68e776527485c1c5014429470a438ade09
Devon Smith <smithde@oclc.org>
2019-05-07 15:12:52 -0400

    updated to new callback definition syntax

--- lib/sugar/templates/engine.ex
+++ lib/sugar/templates/engine.ex
@@ -3,7 +3,6 @@ defmodule Sugar.Templates.Engine do
   Specification of the template engine API implemented
   by individual template engines.
   """
-  use Behaviour
 
   @type template :: Sugar.Templates.Template.t
   @type reason   :: String.t
@@ -16,10 +15,10 @@ defmodule Sugar.Templates.Engine do
   source from the struct if it is not needed after compilation but
   retain it if needed for rendering.
   """
-  defcallback compile(template) :: {:ok, template} | {:error, reason}
+  @callback compile(template) :: {:ok, template} | {:error, reason}
 
   @doc """
   Renders a compiled template based on the given `template` struct.
   """
-  defcallback render(template, vars) :: {:ok, iodata}
+  @callback render(template, vars) :: {:ok, iodata}
 end
